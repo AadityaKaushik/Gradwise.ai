7e5c410d1ff78c6950609663a989b2ce5bf94aac
AadityaKaushik <aadityakaushik3606@gmail.com>
2026-03-30 04:51:39 +0530

    added HTTP error raising

--- backend/services/auth_service.py
+++ backend/services/auth_service.py
@@ -17,17 +17,25 @@ def signup_user(email, password):
         "user_id": user_id
     }
 
+from fastapi import HTTPException, status
+
 def login_user(email, password):
     existing_user = get_user_by_email(email)
 
     if not existing_user:
-        raise Exception("Invalid credentials")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials"
+        )
 
     hash_stored = existing_user["password_hash"]
     user_id = existing_user["user_id"]
 
     if not verify_password(password, hash_stored):
-        raise ValueError("Invalid credentials")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials"
+        )
 
     return {
         "message": "Login successful",
